dd5ab2f740b999ba63b0de29bd71eac98b416870
yoesk8 <yoelgutierrez2021@yahoo.com>
2022-07-13 22:47:55 +0000

    Created computerPick function and win function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,32 @@
-$(".rock-btn").click(function(){$("#your-choice").html("You chose Rock!")});
-$(".paper-btn").click(function(){$("#your-choice").html("You chose Paper!")});
-$(".scissors-btn").click(function(){$("#your-choice").html("You chose Scissors!")});
+// Your Choice changing with clicks to match the clicked option
+$(".rock-btn").click(function(){$("#your-choice").html("Rock")});
+$(".paper-btn").click(function(){$("#your-choice").html("Paper")});
+$(".scissors-btn").click(function(){$("#your-choice").html("Scissors")});
+
+// Reset Game function clearing everything
+function resetGame(){ 
+    document.getElementById("your-choice").innerHTML = "";
+    document.getElementById("computer-choice").innerHTML = "";
+}
+//Generates randome Computer Pick
+function computerPick() {
+
+    const picks = ["Rock", "Paper", "Scissors"];
+    const randomNumber = Math.floor(Math.random() * 3);
+     $("#computer-choice").html(picks[randomNumber]);
+     let yourChoice = document.getElementById("your-choice").innerHTML
+     let computerChoice = document.getElementById("computer-choice").innerHTML
+
+     if (yourChoice + computerChoice === "RockScissors"){
+          console.log("You win!")
+    }
+}
+
+//This function is called if you win
+function win() {
+    yourScoreNum++;
+    yourScore.innerHTML = yourScoreNum;
+    result.innerHTML = "YOU WIN!";
+
+}
+
