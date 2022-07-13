d770b2bff9ebfb07cee5c5ce3d5dbacd82f77782
yoesk8 <yoelgutierrez2021@yahoo.com>
2022-07-13 13:22:13 +0000

    Added structure to project

---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+$(".rock-btn").click(function(){$("#your-choice").html("You chose Rock!")});
+$(".paper-btn").click(function(){$("#your-choice").html("You chose Paper!")});
+$(".scissors-btn").click(function(){$("#your-choice").html("You chose Scissors!")});
